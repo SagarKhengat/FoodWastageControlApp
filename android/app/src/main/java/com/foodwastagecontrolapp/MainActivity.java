fabf835d5cba2917e8dc67a30481bcd74ec27217
Sagar Khengat <sagar.khengat@copious.care>
2021-01-23 18:21:37 +0530

    UI Changes

--- android/app/src/main/java/com/foodwastagecontrolapp/MainActivity.java
+++ android/app/src/main/java/com/foodwastagecontrolapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.foodwastagecontrol;
+package com.foodwastagecontrolapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FoodWastageControl";
+    return "FoodWastageControlApp";
   }
 }
--- android/app/src/main/java/com/foodwastagecontrolapp/MainApplication.java
+++ android/app/src/main/java/com/foodwastagecontrolapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.foodwastagecontrol;
+package com.foodwastagecontrolapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.foodwastagecontrol.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.FoodWastageControlApp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
